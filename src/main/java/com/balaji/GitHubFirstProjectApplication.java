26e6f482cabc203cc246c6edb5434c67875df213
BalajiNagargoje <balajinagargoje2410@gmail.com>
2021-10-07 10:28:57 +0530

    IES-1:New git hub project created

---
+++ src/main/java/com/balaji/GitHubFirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.balaji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubFirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubFirstProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/balaji/GitHubFirstProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balaji;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubFirstProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
