b1739c83b30e22f8315d0052fc2ab6dd9cd10dcb
BalajiNagargoje <balajinagargoje2410@gmail.com>
2021-10-07 10:52:42 +0530

    IES-2:controller,Service,Model classes are created with there Methods

---
+++ src/main/java/com/balaji/model/UserEntity.java
@@ -0,0 +1,11 @@
+package com.balaji.model;
+
+
+public class UserEntity {
+	
+	private int userId;
+	private String name;
+	private String address;
+	private long moNo;
+
+}
---
+++ src/main/java/com/balaji/service/UserServiceI.java
@@ -0,0 +1,7 @@
+package com.balaji.service;
+
+public interface UserServiceI {
+	
+	public void userdetails();
+
+}
